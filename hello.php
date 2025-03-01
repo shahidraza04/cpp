ee94df76f9f21dd42cdaaafa96b180e1bdbf7ca3
Md. Shahid Raza <shahidhets1@gmail.com>
2025-03-01 09:26:35 +0530

    Create hello.php

---
+++ hello.php
@@ -0,0 +1,9 @@
+#include<iostream> //header file.
+using namespace std; //name space defined or scope resolution to std.
+int main(){
+    std::cout<<"hello"<<"shahid"<<"\n"; //std::cout(scope resolution to std.) :: console.output.stream
+    cout<<"hellow world"<<"\n";
+    cout<<"BBA(CA)"<<"\n";
+    cout<<"age(20)"<<"\n";
+    return 0;
+}
