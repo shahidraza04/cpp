d0dab46700f837a1b3aa02e85b6725c248ec794f
Md. Shahid Raza <shahidhets1@gmail.com>
2025-03-01 09:27:14 +0530

    Create input-output.cpp

---
+++ input-output.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main (){
+    // //output
+    //  cout<<"for output and display"<<"\n";
+
+    //  //input
+    //  int num;
+    //  cout<<"enter the number"<<"\n";
+    //  cin >>num;
+
+//arithematic
+     int num1, num2;
+    cout<<"enter first number:"<<"\n";
+    cin >>num1;
+
+    cout<<"enter secount number:"<<"\n";
+    cin >>num2;
+
+    int sum = num1 + num2;
+    cout <<"num1 + num2 =" <<num1 + num2<<"\n";
+    return 0;
+
+    
+} 
