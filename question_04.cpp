22ad90324c106a841bf08070c7d35b32a8e14210
Md. Shahid Raza <shahidhets1@gmail.com>
2025-03-01 09:36:45 +0530

    Create question_04.cpp

---
+++ question_04.cpp
@@ -0,0 +1,23 @@
+//program to print right oriented right angled pyramid.
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n;
+ 
+    printf("Enter the number of rows: ");
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i++) {
+        
+        for (int j = 1; j <= n - i; j++) {
+            printf(" ");
+        }
+        
+        for (int j = 1; j <= i; j++) {
+            printf("%d", j);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
