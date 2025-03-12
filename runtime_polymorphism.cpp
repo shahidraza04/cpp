9804e10f1a5700428de6082fed540b7842ca8122
Md. Shahid Raza <shahidhets1@gmail.com>
2025-03-12 09:51:11 +0530

    Create runtime_polymorphism.cpp

---
+++ runtime_polymorphism.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Animal
+{
+    public :
+    void makesound (){
+        cout<< "Dog Barks"<<endl;
+    }
+};
+class Dog : public Animal{
+ public : void makesound (){
+    cout <<"Dog barks"<<endl;
+ }
+};
+class Cat : public Animal{
+    public : void makesound (){
+        cout <<"cat Meows"<<endl;
+    }
+};
+
+int main(){
+
+    Animal animal;
+
+    Dog dog;
+    Cat cat;
+    animal.makesound();
+    dog.makesound();  cat.makesound();
+    return 0;
+
+}
